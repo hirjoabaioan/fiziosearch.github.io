96f3d39b00c7c72e3604884628a7ae57c9393a75
Ioan Hîrjoabă <hirjoaba.ioan@gmail.com>
2022-05-15 22:53:09 +0300

    bug

--- static/js/therapist-data.js
+++ static/js/therapist-data.js
@@ -7,7 +7,7 @@ let therapists = [{
     "address": "Sector 1, Sector 3, Sector 4, Universitate",
     "profession": "Fizioterapeut",
     "specialization": "Recuperare traumatică",
-    "exp": "01.06.2020",
+    "exp": "01.01.2020",
     "reviews": "0",
     "courses": ["Dynamic tape", "IASTM", "Masaj", "Kinesiotaping", "Cupping",],
     "about": "Sunt o persoană sociabilă, ambițioasă, serioasă și consecventă. Mă adaptez ușor și îmi place să lucrez cu oamenii. Manifest răbdare cu cei din jurul meu și sunt implicat în proiectele mele.",
@@ -23,7 +23,7 @@ let therapists = [{
     "address": "Sector 1, Sector 2, Sector 3",
     "profession": "Fizioterapeut",
     "specialization": "Recuperare pediatrică",
-    "exp": "01.06.2012",
+    "exp": "01.01.2012",
     "reviews": "0",
     "courses": ["Schroth", "Vojta",],
     "about": "Pasionată de ceea ce fac, supraspecializată in terapia Schroth și mă implic trup și suflet în recuperarea pacienților. Rol important în vindecarea afecțiunilor și traumatismelor copiilor, fiind de asemenea un sprijin mental pe toată durata recuperarii.",
@@ -47,7 +47,7 @@ let therapists = [{
 },
 
 { 
-    "id": "",
+    "id": "4",
     "picture": "",
     "name": "Chivu Alexandra",
     "phoneNumber": "0720196197",
