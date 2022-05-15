7f57ce0dd8c72e1e49cf84fb3bb43f096f657b61
Ioan Hîrjoabă <hirjoaba.ioan@gmail.com>
2022-05-15 12:44:15 +0300

    Delete static directory

--- static/js/home.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $("button").click(function(){
-        $("#test").hide();
-        $("#nav-placeholder").hide();
-        // $("#nav-placeholder").load("../../templates/nav.html");
-    });
-});
