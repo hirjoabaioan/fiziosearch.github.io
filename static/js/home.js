1b7a8c2bdde188018a8e55f1873594062421359f
Ioan Hîrjoabă <hirjoaba.ioan@gmail.com>
2022-05-28 15:18:37 +0300

    update

--- static/js/home.js
+++ static/js/home.js
@@ -5,3 +5,58 @@ $(document).ready(function(){
         // $("#nav-placeholder").load("../../templates/nav.html");
     });
 });
+
+//OLD
+// function searchTherapists(){
+//     var foundTherapists = [];
+//     var searchItem = $('#therapist-name').val();
+//     var tsList = [];
+//     var id = therapists.id; // verifica sa nu existe dubluri inainte de a afisa rezultatele
+//     if(searchItem){
+
+//       //cautat dupa nume
+//       therapists.forEach(function(therapist){
+//         var fName = therapist.name;
+//         if (searchItem === fName){
+//           foundTherapists.push(therapist);
+//         }
+//       });
+
+//       //cautat dupa locatie
+//       therapists.forEach(function(therapist){
+//         var fAddress = therapist.address;
+//         for(var loc = 0; loc<fAddress.length; loc++){
+//           if(searchItem === fAddress[loc])
+//             foundTherapists.push(therapist);
+//         }
+//       });
+
+        
+//       //cautat dupa sector 
+//       therapists.forEach(function(therapist){
+//         var keys = Object.getOwnPropertyNames(locations);
+//         keys.forEach(i =>{
+//           var fAddress = therapist.address;
+//             if(i === searchItem){
+//               for(var terLoc = 0; terLoc<fAddress.length; terLoc++){
+//                 for(var j = 0; j<locations[i].length; j++){
+//                   if(locations[i][j] === fAddress[terLoc]){
+//                       foundTherapists.push(therapist);
+//                   }
+//                 }
+//               }
+//             }
+//         });
+//       });
+//     }
+//     else {
+//         foundTherapists = therapists;
+//     }
+//     // Elimina dublurile
+//     foundTherapists = foundTherapists.filter((value, index, self) => index === self.findIndex((t) => (t.id === value.id)));
+    
+//     displayTherapists(foundTherapists.sort(()=> Math.random() - 0.5));
+//     showTherapistData(foundTherapists);
+// }
+
+
--- static/js/index.js
+++ static/js/index.js
@@ -39,55 +39,55 @@ function namesList (){
 
 
 function searchTherapists(){
-    var foundTherapists = [];
-    var searchItem = $('#therapist-name').val();
-    var tsList = [];
-    var id = therapists.id; // verifica sa nu existe dubluri inainte de a afisa rezultatele
-    if(searchItem){
+  var foundTherapists = [];
+  var searchItem = $('#therapist-name').val();
 
-      //cautat dupa nume
-      therapists.forEach(function(therapist){
-        var fName = therapist.name;
-        if (searchItem === fName){
+  if(searchItem){
+    //cautat dupa nume
+    searchItem = searchItem.toLowerCase();
+    therapists.forEach(function(therapist){
+      var fullName = (therapist.name + ' ' + therapist.prename).toLowerCase();
+      var match = fullName.includes(searchItem);
+      if (match){
+        foundTherapists.push(therapist);
+      }
+    });
+
+    //cautat dupa locatie
+    therapists.forEach(function(therapist){
+      var fAddress = therapist.address;
+      for(var loc = 0; loc<fAddress.length; loc++){
+        if(fAddress[loc].toLowerCase().includes(searchItem))
           foundTherapists.push(therapist);
-        }
-      });
+      }
+    });
 
-      //cautat dupa locatie
-      therapists.forEach(function(therapist){
+    //cautat dupa sector 
+    therapists.forEach(function(therapist){
+      var keys = Object.getOwnPropertyNames(locations);
+      keys.forEach(i =>{
         var fAddress = therapist.address;
-        for(var loc = 0; loc<fAddress.length; loc++){
-          if(searchItem === fAddress[loc])
-            foundTherapists.push(therapist);
-        }
-      });
-
-        
-      //cautat dupa sector 
-      therapists.forEach(function(therapist){
-        var keys = Object.getOwnPropertyNames(locations);
-        keys.forEach(i =>{
-          var fAddress = therapist.address;
-            if(i === searchItem){
-              for(var terLoc = 0; terLoc<fAddress.length; terLoc++){
-                for(var j = 0; j<locations[i].length; j++){
-                  if(locations[i][j] === fAddress[terLoc]){
-                      foundTherapists.push(therapist);
-                  }
+          if(i === searchItem){
+            for(var terLoc = 0; terLoc<fAddress.length; terLoc++){
+              for(var j = 0; j<locations[i].length; j++){
+                if(locations[i][j] === fAddress[terLoc]){
+                    foundTherapists.push(therapist);
                 }
               }
             }
-        });
+          }
       });
-    }
-    else {
-        foundTherapists = therapists;
-    }
-    // Elimina dublurile
-    foundTherapists = foundTherapists.filter((value, index, self) => index === self.findIndex((t) => (t.id === value.id)));
-    
-    displayTherapists(foundTherapists.sort(()=> Math.random() - 0.5));
-    showTherapistData(foundTherapists);
+    });
+  }
+  else {
+      foundTherapists = therapists;
+  }
+
+  // Elimina dublurile
+  foundTherapists = foundTherapists.filter((value, index, self) => index === self.findIndex((t) => (t.id === value.id)));
+  
+  displayTherapists(foundTherapists.sort(() => Math.random() - 0.5));
+  showTherapistData(foundTherapists);
 }
 
 
