772872e46e3665ff8e08e63ec29b419e52a98ac2
Ioan Hîrjoabă <hirjoaba.ioan@gmail.com>
2022-06-06 01:31:25 +0300

    Updates

--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,44 @@
+// Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('nav').outerHeight();
+
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 150);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+    var h = 150;
+    
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('nav').removeClass('nav-d').addClass('nav-u');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()+h) {
+            $('nav').removeClass('nav-u').addClass('nav-d');
+        }
+    }
+    
+    lastScrollTop = st;
+}
+
+
 var windowCheck = true;
 var checkText = "Caută un terapeut în funcție de sectorul sau cartierul în care locuiești!";
 var search = $('#therapist-name');
