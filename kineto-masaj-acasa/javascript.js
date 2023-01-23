7a9a2367268fee8d39d9964e15252f1c967a879c
Ioan Hîrjoabă <hirjoaba.ioan@gmail.com>
2023-01-23 21:49:23 +0200

    Mesagerie (done)

--- kineto-masaj-acasa/javascript.js
+++ kineto-masaj-acasa/javascript.js
@@ -1,66 +1,60 @@
 $(document).ready(function(){
 
-if (top.location.pathname === '/templates/search.html')
-{
 
-$('.contact-info').hide();
+        $('.contact-info').hide();
 
-$(window).resize (function (){
-    if($(window).width() < 750 && $('.list-container').is(":visible")){
-        $('.info-container').hide();
-    }else if($(window).width() > 751) {
-        $('.info-container').show();
-    }
+        $(window).resize (function (){
+            if($(window).width() < 750 && $('.list-container').is(":visible")){
+                console.log("work1");
+                $('.info-container').hide();
+            }else if($(window).width() > 751) {
+                $('.info-container').show();
+                console.log("work2");
+            }
 
-})
+        })
 
-if(window.matchMedia("(max-width: 47rem)").matches){
-    $('.info-container').hide();
-}
+        if(window.matchMedia("(max-width: 47rem)").matches){
+            $('.info-container').hide();
+        }
 
-if(window.matchMedia("(hover:hover) and (pointer:fine)").matches){
-    $('.med').removeClass('btn btn-secondary');
-}
+        if(window.matchMedia("(hover:hover) and (pointer:fine)").matches){
+            $('.med').removeClass('btn btn-secondary');
+        }
 
-$('.contact-button').click(function(){
-    $('.contact-info').show();
-    $(this).hide();
-});
-
-
-function showTherapistInfo() {
-    if(window.matchMedia("(max-width: 47rem)").matches) {
-        $('.list-container').removeClass('d-flex');
-        $('.list-container').hide();
-    }
-    $('.info-container').show();
-    $('.info-container').addClass('grid');
-
-}
-
-function closeTherapistInfo() {
-    if(window.matchMedia("(max-width: 47rem)").matches){
-        $('.list-container').addClass('d-flex');
-        $('.list-container').show();
-        $('.info-container').removeClass('grid');
-        $('.info-container').hide();
-    }
-
-    if(window.matchMedia("(min-width: 47rem)").matches){
-        var replace = `
-        <div class="grid jusify-content-start align-items-center" style="width: 500px;"  id="startPage">
-            <span id="txtCheck">Alege un terapeut din listă</span>
-        </div>
-        `;
-    
-        $('.info-container').html(replace);
-    }
-
-}
-
-}
+        $('.contact-button').click(function(){
+            $('.contact-info').show();
+            $(this).hide();
+        });
 
 
+        $('.person-box').click(function() {
+            if(window.matchMedia("(max-width: 47rem)").matches) {
+                $('.list-container').removeClass('d-flex');
+                $('.list-container').hide();
+            }
+            $('.info-container').show();
+            $('.info-container').addClass('grid');
+        });
+
+        $('#close').click(function() {
+            if(window.matchMedia("(max-width: 47rem)").matches){
+                $('.list-container').addClass('d-flex');
+                $('.list-container').show();
+                $('.info-container').removeClass('grid');
+                $('.info-container').hide();
+            }
+
+            if(window.matchMedia("(min-width: 47rem)").matches){
+                var replace = `
+                <div class="grid jusify-content-start align-items-center" style="width: 500px;"  id="startPage">
+                    <span id="txtCheck">Alege un terapeut din listă</span>
+                </div>
+                `;
+            
+                $('.info-container').html(replace);
+            }
+        });
 })
 
 
